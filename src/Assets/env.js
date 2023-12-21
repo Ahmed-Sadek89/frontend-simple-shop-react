28a68048e70b263aa8bf1871e7df40ddddca580f
EL_SADEK <81380165+Ahmed-Sadek89@users.noreply.github.com>
2023-12-21 16:13:02 +0200

    Update env.js
    change the backend link

--- src/Assets/env.js
+++ src/Assets/env.js
@@ -1,2 +1,2 @@
-export const apiLink = 'https://backend-shop-node.herokuapp.com/api'
+export const apiLink = 'https://simple-shop.onrender.com/api'
 export const PUBLIC_KEY = "pk_test_51L736ME3s8qpCTylLMf8wF9XKYdcvuRfwLy1pVvt7MvByKBrZQu360smy3uW3QR5Lt95zLFwBUkKvpVxG6PxAQ9400QRzlGyyT";
